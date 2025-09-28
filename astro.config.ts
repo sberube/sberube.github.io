6a02abc37eea578a6687175f27bf66b8b2e831d7
Simon Berube <berube.simon@gmail.com>
2025-09-27 23:16:24 -0400

    feat: add reading time

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
+import { remarkReadingTime } from './remark-reading-time.mjs';
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
@@ -20,7 +21,7 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }], remarkReadingTime],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
